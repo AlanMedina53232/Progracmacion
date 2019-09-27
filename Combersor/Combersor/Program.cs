02555f125068443c5f5d0eccf759d14caefeb34e
AlanMedina53 <42563570+AlanMedina53232@users.noreply.github.com>
2019-09-26 22:53:15 -0700

    AlanMedina53232

---
+++ Combersor/Combersor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Combersor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tc, tp, tc2;
+            Console.Write("Ingrese la cantidad de centavos: ");
+            tc = Convert.ToInt32(Console.ReadLine());
+            Calcular(tc, out tp, out tc2);
+
+            Console.WriteLine("El resultado es: " + tp + " " + " Pesos " + "\t" + tc2 + " " + " Centavos: ");
+
+
+
+            void Calcular (int tocentavos, out int pesos, out int centavos)
+            {
+                pesos = tocentavos / 100;
+                centavos = tocentavos % 100;
+
+            }
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Sobre carga/Sobre carga/Operacion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sobre_carga
+{
+    class Operacion
+    {
+        public double Multiplicar()
+        {
+            double m = 5*6;
+            return m;
+
+        }
+        public double Multiplicar(double a)
+        {
+            double m = a * 2;
+            return m;
+
+        }
+        public double Multiplicar(double a, double b)
+        {
+            double m = a * b;
+            return m;
+        }
+    }
+}
