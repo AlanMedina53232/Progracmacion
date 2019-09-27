06af43cd723419580baf25261ade55f1ffa2265c
AlanMedina53 <42563570+AlanMedina53232@users.noreply.github.com>
2019-09-26 22:51:49 -0700

    AlanMedina53232

---
+++ Sobre carga/Sobre carga/Datos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sobre_carga
+{
+    class Datos
+    {
+       public void Obtenerdatos()
+        {
+            Console.WriteLine("Hello");
+            Console.WriteLine("Ingrese dos datos ");
+            Console.Write("\nDato1: ");
+            double dato1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("\nDato2: ");
+            double dato2 = Convert.ToDouble(Console.ReadLine());
+            Operacion op = new Operacion();
+            double res = op.Multiplicar(dato1, dato2);
+            Console.WriteLine("El resultado es: " + res);
+        }
+    }
+}
